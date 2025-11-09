e9f86bf8b0a20021032e542415e12caa24f51bf8
kuawy004 <kuawy004@mymail.unisa.edu.au>
2025-11-09 20:01:39 +1030

    added initial HealthRecord which will manage the animals health

--- animal.py
+++ animal.py
@@ -1,8 +1,16 @@
-'''
+"""
 File: filename.py
 Description: A brief description of this Python module.
 Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+
+from datetime import date
+class HealthRecord:
+    def __init__ (self, description, severity, treatment_notes =" "):
+        self.date = date.today
+        self.description = description
+        self.severity = severity
+        self.treatment_notes = treatment_notes
