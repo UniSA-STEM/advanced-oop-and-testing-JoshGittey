9a42dcaf35395e77e6db03ed619215197f4873ad
kuawy004 <kuawy004@mymail.unisa.edu.au>
2025-11-10 18:41:59 +1030

    fixed some indentation erros

--- animal.py
+++ animal.py
@@ -10,18 +10,47 @@ This is my own work as defined by the University's Academic Integrity Policy.
 from datetime import date
 class HealthRecord:
     def __init__ (self, description, severity, treatment_notes =" "):
-        self.__date = date.today()
-        self.__description = description
-        self.__severity = severity
-        self.__treatment_notes = treatment_notes
+        self.date = date.today()
+        self.description = description
+        self.severity = severity
+        self.treatment_notes = treatment_notes
+
     def __str__(self):
-        return f"{self.__date} - {self.__description} -(Severity: {self.__severity})"
+        return f"{self.date} - {self.description} -(Severity: {self.severity})"
 
 class Animal:
-   def __init__(self, name, species, age, diet):
+
+    def __init__(self, name, species, age, diet):
+       #Encapsulated attributes
         self.__name = name
         self.__species = species
         self.__age = age
         self.__diet = diet
-        self.__health_records = []
+        self.__health_records = [] # list will hold HealthRecord objects
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        if not name:
+            raise ValueError("Name cannot be empty")
+        self.__name = name
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        if age < 0:
+            raise ValueError("Age cannot be negative")
+        self.__age = age
+
+    # Animal behaviours
+    def make_sound(self):
+        pass
+
+    def eat(self):
+        return f"{self.__name} is eating {self.__diet}"
+
+    def sleep(self):
+        return f"{self.__name} is currently sleeping"
+
 
