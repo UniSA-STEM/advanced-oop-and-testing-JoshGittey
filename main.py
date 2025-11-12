36dc871a7c6071c98b7ac09c7dc0b038ea72460e
kuawy004 <kuawy004@mymail.unisa.edu.au>
2025-11-12 16:28:30 +1030

    more main.py script testing

--- main.py
+++ main.py
@@ -11,20 +11,27 @@ from animal import Animal
 
 Simba = Animal(name = "Leo", species= "Lion", age =5, diet= "Carnivore", sound= "Roar")
 Rio = Animal(name= "Rio", species= "Eagle", age= 9, diet= "Carnivore", sound= "Squawk")
+Cody = Animal(name= "Cody", species= "Penguin", age= 2, diet= "Carnivore", sound= "Chirp")
 
 print(Simba)
 print(Rio)
+print(Cody)
 
 print(Simba.make_sound())
 print(Rio.make_sound())
+print(Cody.make_sound())
 
 print(Simba.eat())
 print(Rio.eat())
+print(Cody.eat())
 
 print(Simba.sleep())
 print(Rio.sleep())
+print(Cody.sleep())
 
 Simba.add_health_record(description= "Cut on Paw", severity= "Low", treatment_notes= "Wound has been cleaned and bandage has been applied")
+Cody.add_health_record(description= "Feather loss", severity= "Medium", treatment_notes= "Vitamin supplement has been applied")
 
 print(Simba.show_health_records())
 print(Rio.show_health_records())
+print(Cody.show_health_records())
