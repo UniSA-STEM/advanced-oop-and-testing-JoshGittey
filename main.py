096f61d1fbf759af527bf99ff3bda0a9d6028dc7
kuawy004 <kuawy004@mymail.unisa.edu.au>
2025-11-12 15:51:57 +1030

    test script to see if animal.py is working how i intend it to

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-'''
+"""
 File: filename.py
 Description: A brief description of this Python module.
 Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+
+from animal import Animal
+
+Simba = Animal(name = "Leo", species= "Lion", age =5, diet= "Carnivore", sound= "Roar")
+Rio = Animal(name= "Rio", species= "Eagle", age= 9, diet= "Carnivore", sound= "Squawk")
+
+print(Simba)
+print(Rio)
