c0a7880a28a7695be52aeaf86c2657928beb01db
kuawy004 <kuawy004@mymail.unisa.edu.au>
2025-11-17 17:11:17 +1030

    methods cleaned up and adjusted. string method included to return final state of the encloser

--- enclosure.py
+++ enclosure.py
@@ -34,26 +34,37 @@ class Enclosure:
           raise TypeError("Animal must be an object to be added")
 
       if animal in self.__animals:
-          return f"{animal.species} in enclosure already"
+          return f"{animal.name} is already in this enclosure already"
 
-      if self.check_animal_capacity():
-          self.__animals.append(animal)
-          return f"{animal.name} has been added to the enclosure"
-      else:
-          return f"Enclosure is full, {animal.name} can not be added"
+      if self.__allowed_species and animal.species not in self.__allowed_species:
+          return f"{animal.species} is not allowed in this {self.__biome} enclosure"
 
-    def check_animal_capacity(self):
-        return len(self.__animals) < self.__max_capacity
+      if not self.check_animal_capacity():
+          return f"enclosure is full {animal.name} cannot be added"
+
+      self.__animals.append(animal)
+      return f"Added {animal.name} to the enclosure"
 
     def remove_animal(self, animal):
         if not isinstance(animal, Animal):
             raise TypeError("Animal must be an object to be removed")
+
+        if animal not in self.__animals:
+            return f"{animal.name} is not in this enclosure"
+
         self.__animals.remove(animal)
+        return f"Removed {animal.name} from the enclosure"
+
+    def check_animal_capacity(self):
+        return len(self.__animals) <= self.__max_capacity
 
     def list_animals(self):
         if not self.__animals:
             return f"No animals are in this enclosure ({self.__biome})"
         return "\n".join(f"{animal.name} the ({animal.species}), Age: {animal.age} years old" for animal in self.__animals)
+
+    def __str__(self):
+        return f"Enclosure ({self.__biome}) - {len(self.__animals)}/{self.__max_capacity} animals"
 
 
 
