46be53f4dbfbed20a8952570b53ef101fd2a3296
kuawy004 <kuawy004@mymail.unisa.edu.au>
2025-11-17 18:03:27 +1030

    staff class created with attributes

--- staff.py
+++ staff.py
@@ -1,8 +1,18 @@
-'''
+"""
 File: filename.py
 Description: A brief description of this Python module.
 Author: Billy Bizilis
 ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+from animal import Animal
+from enclosure import Enclosure
+
+class Staff:
+    def __init__(self, name, age, role):
+        self.__name = name
+        self.__age = age
+        self.__role = role
+        self.__assigned_enclosures = []
+        self.__assigned_enclosures = []
